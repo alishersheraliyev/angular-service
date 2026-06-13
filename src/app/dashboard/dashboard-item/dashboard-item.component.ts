36519a8dea9441568091ae631c1bcf81ab395050
Alisher Sheraliyev <alishersheraliyev18@gmail.com>
2026-06-13 20:19:01 +0500

    some changes

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  // image = input<{ src: string; alt: string }>({ required: true });
+  // title = input<string>({ required: true });
+
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
+}
